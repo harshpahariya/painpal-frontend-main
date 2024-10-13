ca1bbfb0c850fa6e6dbf99125e4940ebf0d4a0b7
Harsh Pahariya <harsh.pahariya78@gmail.com>
2024-10-13 19:30:40 +0530

    Update configure-apis.ts

--- src/api/configure-apis.ts
+++ src/api/configure-apis.ts
@@ -1,5 +1,5 @@
-export const FETCH_PAIN_QUESTIONS = `${(import.meta as any).env.VITE_BASE_URL || 'http://localhost:8080'}/fetch`;
-export const FETCH_PAIN_RESULT = `${(import.meta as any).env.VITE_BASE_URL || 'http://localhost:8080'}/insert`;
+export const FETCH_PAIN_QUESTIONS = `${(import.meta as any).env.VITE_BASE_URL || 'https://painpal-backend.onrender.com/fetch`;
+export const FETCH_PAIN_RESULT = `${(import.meta as any).env.VITE_BASE_URL || 'https://painpal-backend.onrender.com/insert`;
 
 
 
